e6ca2ce81120bdd27bd9fa23728008ada181c3b8
CharlesXu1124 <xuzheyuan961124@gmail.com>
2023-12-02 21:26:36 -0800

    use pointcloud distance instead

--- ros_object_detection.py
+++ ros_object_detection.py
@@ -1,38 +1,21 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-import openai
 import numpy as np
-import os
-import cv2
 from yolov8 import YOLOv8
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import String
 from std_msgs.msg import Header
 from cv_bridge import CvBridge
 
-import requests
-from PIL import Image as PIL_Image
 from numpy import asarray
-import torch
-import array
 import math
 import struct
 
 class RosYolov8Node(Node):
     def __init__(self):
         super().__init__("ros_memeye_node")
-        self.class_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-            'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-            'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-            'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-            'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-            'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-            'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-            'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
-            'scissors', 'teddy bear', 'hair drier', 'toothbrush']
         self.yolo_publisher = self.create_publisher(Float32MultiArray, '/yolo_result', 10)
         self.img_subscription = self.create_subscription(
             Image,
@@ -58,6 +41,7 @@ class RosYolov8Node(Node):
         self.yolov8_detector = YOLOv8(self.model_path, conf_thres=0.5, iou_thres=0.5)
         self.get_logger().info("============Yolo Model Ready===========")
         self.bridge = CvBridge()
+        self.point_cloud = PointCloud2()
         self.depth_map = np.zeros((480, 640), np.float32)
 
         # camera parameters
@@ -75,10 +59,8 @@ class RosYolov8Node(Node):
         return point_cloud
 
     def img_callback(self, Image):
-        # self.get_logger().info("image received")
         cv_image = self.bridge.imgmsg_to_cv2(Image, desired_encoding='passthrough')
         boxes, scores, labels = self.yolov8_detector(cv_image)
-        results = ""
 
         yolo_msg = Float32MultiArray()
 
@@ -87,45 +69,53 @@ class RosYolov8Node(Node):
         header.frame_id = 'base_link'  # Change this frame_id as needed
         points = []
         
-        # print(boxes, end="\r")
-
+        if len(self.point_cloud.data) == 0:
+            return
 
         for box, score, label in zip(boxes, scores, labels):
             x1, y1, x2, y2 = box.astype(int)
             center_x = int((x1 + x2) / 2.0)
             center_y = int((y1 + y2) / 2.0)
-            # https://stackoverflow.com/questions/31265245/extracting-3d-coordinates-given-2d-image-points-depth-map-and-camera-calibratio
-            # compute x, y, z of the detected object
-            x = (center_x - self.cx) * self.depth_map[center_y][center_x] / self.fx
-            y = (center_y - self.cy) * self.depth_map[center_y][center_x] / self.fy
-            z = self.depth_map[center_y][center_x]
+            offset = center_y * 640 + center_x
 
-            points.append(float(x))
-            points.append(float(y))
-            points.append(float(z))
+            x = struct.unpack("f", bytes([self.point_cloud.data[offset * 16],
+                 self.point_cloud.data[offset * 16 + 1],
+                 self.point_cloud.data[offset * 16 + 2],
+                 self.point_cloud.data[offset * 16 + 3]
+            ]))
+
+            y = struct.unpack("f", bytes([self.point_cloud.data[offset * 16 + 4],
+                 self.point_cloud.data[offset * 16 + 5],
+                 self.point_cloud.data[offset * 16 + 6],
+                 self.point_cloud.data[offset * 16 + 7]
+            ]))
+
+            z = struct.unpack("f", bytes([self.point_cloud.data[offset * 16 + 8],
+                 self.point_cloud.data[offset * 16 + 9],
+                 self.point_cloud.data[offset * 16 + 10],
+                 self.point_cloud.data[offset * 16 + 11]
+            ]))
+
+            if math.isnan(x[0]) or math.isnan(y[0]) or math.isnan(z[0]):
+                continue
+
+            points.append(float(x[0]))
+            points.append(float(y[0]))
+            points.append(float(z[0]))
             points.append(float(label))
 
         yolo_msg.data = points
-        # print(points)
-
-        self.yolo_publisher.publish(yolo_msg)            
-
-    def depth_callback(self, Image):
-        self.depth_map = self.bridge.imgmsg_to_cv2(Image, desired_encoding='passthrough') / 1000.0
+        self.yolo_publisher.publish(yolo_msg)
 
     def points_callback(self, points):
-        print(points.data[0:4])
-
-
+        self.point_cloud = points
 
 def main(args=None):
-    
     rclpy.init(args=args)
     RosYoloNode = RosYolov8Node()
     rclpy.spin(RosYoloNode)
     RosYoloNode.destroy_node()
     rclpy.shutdown()
-    
 
 if __name__ == '__main__':
     main()
